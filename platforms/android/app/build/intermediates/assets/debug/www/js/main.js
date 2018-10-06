27d844320a824902767c4a261a076e27235c33af
petrix <yuri.petrik@gmail.com>
2018-10-07 01:02:04 +0300

    xxx

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  screen.lockOrientation('portrait');
+
+
+  $('audio').data('bpm','126');
+  $('.spinner-wrap').click(function(){
+    var $this =$(this),audio = $this.siblings('audio')[0],bpm = $this.siblings('audio').data('bpm');
+    console.log(bpm);
+    if(audio.paused){
+      audio.play();
+      $this.addClass('playing');
+    }else{
+      audio.pause();
+      $this.removeClass('playing');
+    }
+  });
+});

db24c7eb6d5f0782240c4e0aef382478735182da
p3xx <yuri.petrik@gmail.com>
2018-10-07 03:24:02 +0300

    xxx

--- www/js/main.js
+++ www/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $('audio').data('bpm','126');
   $('.spinner-wrap').click(function(){
     var $this =$(this),audio = $this.siblings('audio')[0],bpm = $this.siblings('audio').data('bpm');
-    console.log(bpm);
+    // console.log(bpm);
     if(audio.paused){
       audio.play();
       $this.addClass('playing');

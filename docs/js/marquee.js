91a45872b0e8750818da7dbc199e97b528b10c5b
petrix <yuri.petrik@gmail.com>
2018-10-13 03:54:39 +0300

    xxx

--- docs/js/marquee.js
+++ docs/js/marquee.js
@@ -1,36 +1,43 @@
 $(document).ready(function () {
   var intId;
-  var windowWidth, moduleWidth, titleWidth;
-  var ww, w , yLength;
-  var step = 180;
+  var moduleWidth, titleWidth;
+  var step = 150;
   // }
   titleWidth = $('.songtitle').width();
   moduleWidth = $('.songmodule').width();
-  slideLeft = titleWidth + moduleWidth;
+  slideLeft = moduleWidth;
 
 
   var intervalId = function () {
 
     $(window).resize(function () {
-      $('.songtitle').toggleClass('opacity');
+      $('.songtitle').removeClass('opacity');
       moduleWidth = $('.songmodule').width();
       // w = $('.songmodule').width();
+      titleWidth = $('.songtitle').width();
       slideLeft = moduleWidth;
     });
-    console.log(slideLeft);
+    // console.log("slideLeft = " + slideLeft);
+    // console.log("titleWidth ="+ titleWidth);
+    // console.log("moduleWidth = "+ moduleWidth);
+
+
     // var yLength = ww + $('.songtitle').html().length * (parseFloat($('.songtitle').css('font-size'))) / 1.7;
-    slideLength = moduleWidth + titleWidth;
+    // slideLength = moduleWidth + titleWidth;
     // yLength = w + ($('.songtitle').width()+(w/10));
-    if ((slideLeft + (slideLength - moduleWidth)) > 0) {
-      $('.songtitle').removeClass('opacity');
+    if ((slideLeft + $('.songtitle').width()+ step>0)) {
+      if( slideLeft < $('.songmodule').width()){
+        $('.songtitle').addClass('opacity');
+      }
+      
       $('.songtitle').css('transform', 'translateX(' + slideLeft + 'px)');
     } else {
-      $('.songtitle').addClass('opacity');
+      $('.songtitle').removeClass('opacity');
       $.getJSON('http://radiobratan.tk:88/api/nowplaying', function (data) {
         $('.songtitle').html(data[0].now_playing.song.text);
-        titleWidth = $('.songtitle').width();
-        slideLength = moduleWidth + titleWidth;
-        slideLeft = moduleWidth + step;
+        // titleWidth = $('.songtitle').width();
+        slideLeft = $('.songmodule').width();
+        // slideLeft = moduleWidth + step;
       });
     }
     slideLeft -= step;
